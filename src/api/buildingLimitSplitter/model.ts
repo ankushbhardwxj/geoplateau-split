e83697661f07a3d0f9be07cb58e616d10017d615
Ankush Bhardwaj <40923324+ankushbhardwxj@users.noreply.github.com>
2024-09-02 09:28:31 +0200

    fix: update model schema

--- src/api/buildingLimitSplitter/model.ts
+++ src/api/buildingLimitSplitter/model.ts
@@ -23,9 +23,28 @@ const buildingLimitSplitterSchema = new mongoose.Schema(
   {
     versionKey: "__v",
     optimisticConcurrency: true,
-    strict: false,
   },
 );
+
+// mongoose doesn't update __v field for findOneAndUpdate, so this is recommended middleware to update it
+// https://mongoosejs.com/docs/guide.html#versionKey
+buildingLimitSplitterSchema.pre("findOneAndUpdate", async function (next) {
+  const update: any = this.getUpdate();
+  if (update.__v != null) {
+    update.__v = undefined;
+  }
+  const keys = ["$set", "$setOnInsert"];
+  for (const key of keys) {
+    if (update[key] != null && update[key].__v != null) {
+      update[key].__v = undefined;
+      if (Object.keys(update[key]).length === 0) {
+        delete update[key];
+      }
+    }
+  }
+  update.$inc = update.$inc || {};
+  update.$inc.__v = 1;
+});
 
 const BuildingLimitSplitter = mongoose.model("BuildingLimitSplitter", buildingLimitSplitterSchema);
 export default BuildingLimitSplitter;
